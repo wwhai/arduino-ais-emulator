8e576854ed457002af056f7696543647b4646201
wwhai <cnwwhai@gmail.com>
2024-01-05 13:45:28 +0800

    dev: add GRMC  test

--- src/main.cpp
+++ src/main.cpp
@@ -7,26 +7,14 @@ void setup()
 // pio run -e uno -t upload --upload-port COM4
 void loop()
 {
-  Serial.print("!AIVDO,1,1,,,B00000001:2q8UTHSQ003wi4p6jp,0*69\r\n");
+  Serial.print("$GPRMC,123519,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W*6A\r\n");
+  delay(100);
+  Serial.print("!AIVDM,2,1,2,B,55MsP00kLC7L7R?v8d4d<fn\r\n");
   delay(100);
   Serial.print("!AIVDM,1,1,,A,C6:aq@@0822o1cTHJgb=swhPjbhBL@bOSeeUd0000000S5063TBP,0*5A\r\n");
-  delay(100);
-  Serial.print("!AIVDO,1,1,,,B00000001:2q8V4HSQ403wiTp6jp,0*6E\r\n");
-  delay(100);
-  Serial.print("!AIVDO,1,1,,,B00000000r2q8VTHSQ403wj4p6jp,0*24\r\n");
-  delay(100);
-  Serial.print("!AIVDM,1,1,,A,B6:cll000:2m4t4HAuR;Cwv41P06,0*2E\r\n");
-  delay(100);
-  Serial.print("!AIVDM,1,1,,B,B6:biRh0?B2rD3THa1LWhOv021Mk,0*33\r\n");
   delay(100);
   Serial.print("!AIVDM,1,1,,B,C6:aq@@0822o1T4HJfr=OwiPjbhBL@bOSeeUd0000000S5063TBP,0*22\r\n");
   delay(100);
   Serial.print("!AIVDM,1,1,,A,B6:biRh0?B2rD3THa1LWhOv021Mk,0*30\r\n");
-  delay(100);
-  Serial.print("!AIVDM,1,1,,B,B6:VeE@0:b2pWm4HSkNB3wh6sP06,0*3A\r\n");
-  delay(100);
-  Serial.print("!AIVDO,1,1,,,B00000001:2q8W4HSQ403wjTp6jp,0*6C\r\n");
-  delay(100);
-  Serial.print("!AIVDM,1,1,,B,16:Wu9i00L`;MQfAQkFq1o?p0000,0*5A\r\n");
   delay(100);
 }
