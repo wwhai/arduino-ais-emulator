ba59952ff20e179f1ca0c2afe28e495ba7585beb
wwhai <cnwwhai@gmail.com>
2024-01-05 19:15:13 +0800

    dev: 删除损坏的AIS报文校验码

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,12 @@ void loop()
 {
   Serial.print("$GPRMC,123519,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W*6A\r\n");
   delay(100);
-  Serial.print("!AIVDM,2,1,2,B,55MsP00kLC7L7R?v8d4d<fn\r\n");
-  delay(100);
   Serial.print("!AIVDM,1,1,,A,C6:aq@@0822o1cTHJgb=swhPjbhBL@bOSeeUd0000000S5063TBP,0*5A\r\n");
   delay(100);
-  Serial.print("!AIVDM,1,1,,B,C6:aq@@0822o1T4HJfr=OwiPjbhBL@bOSeeUd0000000S5063TBP,0*22\r\n");
-  delay(100);
   Serial.print("!AIVDM,1,1,,A,B6:biRh0?B2rD3THa1LWhOv021Mk,0*30\r\n");
+  delay(100);
+  Serial.print("!AIVDO,1,1,,A,13Mum40000000G?vMdHG6Hi2>220S0@0,0*67\r\n");
+  delay(100);
+  Serial.print("!AIVDM,1,1,,B,C6:aq@@0822o1T4HJfr=OwiPjbhBL@bOSeeUd0000000S5063TBP,0*22\r\n");
   delay(100);
 }
